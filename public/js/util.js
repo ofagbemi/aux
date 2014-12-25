a70bc09040b7a379aa610bd0bb9dfe787c964785
Jare Fagbemi <ofagbemi@stanford.edu>
2014-12-25 15:18:09 -0600

    Added setPrefixedStyle function to util

--- public/js/util.js
+++ public/js/util.js
@@ -20,5 +20,23 @@
                 elem.removeEventListener(p[i]+type, callback);
             }
         },
+        
+        /**
+         * Sets vendor prefixed properties on the given element
+         *
+         * @param HtmlElement elem The element to set the style of
+         * @param string property The unprefixed CSS property to set
+         * @param string value The value to set the CSS property to
+         */
+        setPrefixedStyle: function(elem, property, value) {
+            var p = window.util.prefixes;
+            for(var i = 0; i < p.length; i++) {
+                var prefix = p[i];
+                if(p[i] !== '') {
+                    prefix = '-' + prefix + '-';
+                }
+                elem.style[prefix + type] = value;
+            }
+        },
     };
 }());
